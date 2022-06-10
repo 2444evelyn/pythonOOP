a5ee585beb6f0a71679c6ca309294fdf933e3108
caro-mugo <mugonycaroline@gmail.com>
2022-06-10 20:52:38 +0300

    first commit

--- student.py
+++ student.py
@@ -1,5 +1,15 @@
 class Student:
-    name="Lynn"
-    age=20
-    country="kenya"
-    school="Akirachix"
+     school = "Akirachix"
+     def __init__(self,country,Firstname,Secondname,age):
+        self.Firstname = Firstname
+        self.country = country
+        self.Secondname = Secondname
+        self.age = age
+     def full_name (self):
+
+         return f"Hey {self.Firstname} {self.Secondname} How is {self.country}"
+     def year_of_birth (self):  
+         year=2022 - self.age
+         return f"Hey {self.Firstname} {self.Secondname} How is {self.country} you are {year}"
+     def initials (self):
+         return f"Hey {self.Firstname} {self.Secondname} How is {self.country} your initials {self.Firstname[0]} {self.Secondname[0]}"
